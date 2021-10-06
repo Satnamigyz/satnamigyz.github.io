5fa382e3d7834b948113a7b3103294ab87c17028
Satnamigyz <zygimantas96@gmail.com>
2021-10-06 11:57:02 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -75,7 +75,7 @@ function readChatbox() {
 
   if (chat.indexOf("The catalyst of alteration contained") > -1) {
     let getItem = {
-      item: chat.match(/\d(\s+([a-zA-Z]+\s+)+)\([^)]*\)/i),
+      item: chat.match(/\d\s([a-zA-Z]+( [a-zA-Z]+)+)\s\([^)]*\)),
       time: new Date()
     };
     console.log(getItem);
